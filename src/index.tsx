d94d87100621cb5d6fe052d0396de0e7c1261dbb
André Felipe Rodrigues de Freitas <andre.freitas@hp.com>
2022-02-02 18:25:39 -0300

    create header

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { theme } from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
